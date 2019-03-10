daebf58ba2ecf7de1078145d4a367ebcd88d0e0f
CruOzer <ChrisKost94@gmail.com>
2019-03-10 11:25:19 +0100

    Navigation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const mongoose = require('mongoose');
@@ -30,6 +31,11 @@ mongoose.connect(process.env.MONGO_URI, {
         console.log('MongoDB connection failure: ' + err);
     });
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 // Cookie-Parser middleware
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.send('It works');
+    res.render('index/welcome');
 });
 
+router.get('/dashboard', (req, res) => {
+    res.send('It dashboards');
+});
 
 module.exports = router;
